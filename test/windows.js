370ff237490d85d6214740ee4d6c1c563f336310
David Luecke <daff@neyeon.com>
2012-10-14 11:28:35 -0600

    Windows launching works

--- test/windows.js
+++ test/windows.js
@@ -1,5 +1,9 @@
 var launch = require('../lib');
+var win = require('../lib/local/platform/windows');
 
 launch.local(function(err, local) {
 	console.log(local.browsers);
+	local.firefox('http://github.com', function(error, instance) {
+
+	});
 });
