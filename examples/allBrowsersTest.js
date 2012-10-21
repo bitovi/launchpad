467326eecfa61d79ec06bc8ca0328ed769d0c428
David Luecke <daff@neyeon.com>
2012-10-21 00:28:03 -0600

    Server can launch, list and stop local instances.

--- examples/allBrowsersTest.js
+++ examples/allBrowsersTest.js
@@ -3,18 +3,7 @@ var browsers = ['firefox', 'ie'];
 
 launch.local(function (err, local) {
 	console.log(local.browsers);
-	browsers.forEach(function (browser) {
-		if (local[browser]) {
-			local[browser]('http://ebay.ca', function (err, instance) {
-				// console.log(arguments);
-				instance.on('stop', function (msg) {
-					console.log('Local ' + browser + ' instance stopped', msg);
-				});
-
-				setTimeout(function () {
-					instance.stop();
-				}, 5000);
-			});
-		}
-	});
+	local.phantom('http://github.com', function(error, instance) {
+		console.log(arguments);
+	})
 });
