ec221ebe94f62c44833d4f7d07aa8619942fb469
David Luecke <daff@neyeon.com>
2012-11-17 13:49:18 -0700

    Adds default location for MacOS browsers. Should close #1

--- examples/allBrowsersTest.js
+++
@@ -1,9 +0,0 @@
-var launch = require('../lib');
-var browsers = ['firefox', 'ie'];
-
-launch.local(function (err, local) {
-	console.log(local.browsers);
-	local.phantom('http://github.com', function(error, instance) {
-		console.log(arguments);
-	})
-});
