ef605ff35b517288cbde3d280a603cdaa498937e
David Luecke <daff@neyeon.com>
2013-09-16 23:00:10 -0600

    More work on discovery refactoring.

--- test/macos.js
+++ test/macos.js
@@ -3,19 +3,9 @@ var expect = require('expect.js');
 
 describe('MacOS browser discovery', function() {
 	it('Firefox', function(done) {
-
-		for(var key in macos) {
-			console.log(key);
-		}
 		macos.getBrowser('phantom').then(function(browser) {
-			macos.getBrowser('phantom').then(function(browser) {
-				console.log(browser);
-				done();
-			}, function(error) {
-				console.error(error);
-			});
-		}, function(error) {
-			console.error(error);
+			console.log(browser);
+			done();
 		});
 	});
 });
