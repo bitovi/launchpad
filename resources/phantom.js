a862cd5744c81cbd784e253b0cbf873f40619b2d
David Luecke <daff@neyeon.com>
2014-11-04 16:24:40 -0700

    Print PhantomJS errors and console.logs.

--- resources/phantom.js
+++ resources/phantom.js
@@ -1,4 +1,21 @@
 // A PhantomJS sript that simply opens the web page passed as the command line parameter
 var page = require('webpage').create();
-page.open(phantom.args[0], function (status) {
-});
+
+page.onError = function(msg, trace) {
+  var msgStack = ['ERROR: ' + msg];
+  if (trace && trace.length) {
+    msgStack.push('TRACE:');
+    trace.forEach(function(t) {
+      msgStack.push(' -> ' + t.file + ': ' + t.line + (t.function ? ' (in function "' + t.function +'")' : ''));
+    });
+  }
+
+  console.error(msgStack.join('\n'));
+};
+
+page.onConsoleMessage = function(msg, lineNum, sourceId) {
+  var tail = sourceId ? ' (' + sourceId + ':' + (lineNum || 0) + ')' : '';
+  console.log('CONSOLE: ' + msg + tail);
+};
+
+page.open(phantom.args[0], function () {});
